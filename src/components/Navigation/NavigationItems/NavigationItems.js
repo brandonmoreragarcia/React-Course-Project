f0918e413c33530848500235bea5c5d71c0b9286
brandon <brandonmoreragarcia@gmail.com>
2019-10-11 13:59:45 -0600

    app deployed

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,14 +6,14 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/" exact >Burger Builder</NavigationItem>
-        {props.isAuthenticated 
-            ? <NavigationItem link="/orders" >Orders</NavigationItem> 
+        {props.isAuthenticated
+            ? <NavigationItem link="/orders" >Orders</NavigationItem>
             : null}
-       { props.isAuthenticated 
-       ?  <NavigationItem link="/logout" >Log out </NavigationItem>
-        : <NavigationItem link="/auth" >Authenticate</NavigationItem>
-    }
-            
+        {props.isAuthenticated
+            ? <NavigationItem link="/logout" >Log out </NavigationItem>
+            : <NavigationItem link="/auth" >Authenticate</NavigationItem>
+        }
+
     </ul>
 );
 
